d0af56847862096cf8732d3e2d5b3dd43efdfd5d
Kyaw Zin Hein <kyawzinhein6@gmail.com>
2025-04-11 16:01:52 +0630

    Add the button to download qr code

--- src/components/Generate.jsx
+++ src/components/Generate.jsx
@@ -1,8 +1,10 @@
-import { QRCodeSVG } from 'qrcode.react';
-import { useState } from 'react';
+import { Download } from 'lucide-react';
+import { QRCodeCanvas } from 'qrcode.react';
+import { useRef, useState } from 'react';
 
 const Generate = () => {
   const [value, setValue] = useState('');
+  const qrRef = useRef(null);
 
   return (
     <div className='space-y-4'>
@@ -15,9 +17,32 @@ const Generate = () => {
       />
 
       {value && (
-        <div className='flex justify-center p-4 bg-gray-50 rounded-lg'>
-          <QRCodeSVG value={value} size={200} />
-        </div>
+        <>
+          <div className='flex justify-center p-4'>
+            <QRCodeCanvas value={value} size={200} ref={qrRef} />
+          </div>
+          <button
+            onClick={() => {
+              const base64image = qrRef.current.toDataURL('image/png', 1.0);
+
+              // create anchor tag and add it to the DOM
+              const fakeLink = document.createElement('a');
+              fakeLink.style = 'display:none;';
+              fakeLink.download = value;
+              fakeLink.href = base64image;
+              document.body.appendChild(fakeLink);
+
+              // click and remove anchor tag
+              fakeLink.click();
+              document.body.removeChild(fakeLink);
+              fakeLink.remove();
+            }}
+            className='w-full flex items-center justify-center px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors duration-200'
+          >
+            <Download className='w-5 h-5 mr-2' />
+            Download QR Code
+          </button>
+        </>
       )}
     </div>
   );
